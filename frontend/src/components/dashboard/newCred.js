5c153a274961c38d148b7bad0419fde9800551f5
Anthony Huang <anthill499@gmail.com>
2021-11-12 11:25:49 -0500

    12/12A

--- frontend/src/components/dashboard/newCred.js
+++ frontend/src/components/dashboard/newCred.js
@@ -5,16 +5,29 @@ import passwordGenerator from "../../util/passwordGenerator";
 const CredentialForm = () => {
   const [username, setusername] = useState("");
   const [password, setpassword] = useState("");
+  const [strength, setStrength] = useState(10);
+  const [company, setcompany] = useState("");
   const authGlobal = useContext(AuthContext);
 
-  const rerollPassword = () => {
-    return setpassword(passwordGenerator());
+  const rerollPassword = (e, count) => {
+    e.preventDefault();
+    const newPw = passwordGenerator(count);
+    console.log(newPw);
+    return setpassword(newPw);
   };
+
   // Create a post request to create
   return (
     <div className="Credential-Form-Container">
-      <h4>Create a new Credential</h4>
+      <h4 className="Credential-Header">Create a new Credential</h4>
       <form className="Credential-Form">
+        <label htmlFor="company">What Website?</label>
+        <input
+          name="company"
+          value={company}
+          onChange={(e) => setcompany(e.currentTarget.value)}
+          placeholder="Enter a Company Name"
+        />
         <label htmlFor="username">Username</label>
         <input
           name="username"
@@ -27,12 +40,52 @@ const CredentialForm = () => {
           name="password"
           value={password}
           onChange={(e) => setpassword(e.currentTarget.value)}
-          placeholder="Enter a Password"
+          placeholder="Generate a Password"
+          readOnly={true}
         />
-        <button onClick={() => rerollPassword()}>
+        <label>Password Strength</label>
+        <div className="radio">
+          <label>
+            <input
+              type="radio"
+              value={5}
+              onChange={(e) => setStrength(e.currentTarget.value)}
+            />
+            Weak
+          </label>
+          <label>
+            <input
+              type="radio"
+              value={10}
+              onChange={(e) => setStrength(e.currentTarget.value)}
+            />
+            Good
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value={15}
+              onChange={(e) => setStrength(e.currentTarget.value)}
+            />
+            Strong
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value={25}
+              onChange={(e) => setStrength(e.currentTarget.value)}
+            />
+            Very Strong
+          </label>
+        </div>
+
+        <button onClick={(e) => rerollPassword(e, strength)}>
           Generate Random Password
         </button>
-        <button>Submit</button>
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
--- frontend/src/util/passwordGenerator.js
+++ frontend/src/util/passwordGenerator.js
@@ -3,12 +3,12 @@ const ASCII =
   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
 // Length of ASCII = 93;
 
-function passwordGenerator(count) {
+const passwordGenerator = (count) => {
   let answer = "";
-  while (answer.length !== count) {
+  for (let i = 0; i < count; i++) {
     answer += ASCII[Math.floor(Math.random() * 93)];
   }
   return answer;
-}
+};
 
-module.exports = passwordGenerator;
+export default passwordGenerator;
