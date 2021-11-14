f46e1d67c7c650f4223b423847c573d0dc707e37
Anthony Huang <anthill499@gmail.com>
2021-11-14 14:53:56 -0500

    built

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.NODE_ENV === "production" ? process.env.PORT : 5000;
 // this is a common error, port number
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("/*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
