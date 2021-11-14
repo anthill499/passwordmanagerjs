d46f9e17d9b183882f22e2bdfc9fe977efb278f8
Anthony Huang <anthill499@gmail.com>
2021-11-14 14:57:00 -0500

    built

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const port = process.env.NODE_ENV === "production" ? process.env.PORT : 5000;
 // this is a common error, port number
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend/build")));
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
 
 app.use(express.json());
 app.use(cors());
