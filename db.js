63873f09d93e717f8a57c5610d26ff43bb2042d7
Anthony Huang <anthill499@gmail.com>
2021-11-14 00:53:54 -0500

    dbug

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const dbCred = {
 };
 
 const configuration =
-  !process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "development"
     ? dbCred
     : {
         connectionString: process.env.DATABASE_URL,
--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const app = express();
 const cors = require("cors"); // diff domain apps to interact
 const bodyParser = require("body-parser");
 const path = require("path");
-const port = process.env.NODE_ENV === "production" ? process.env.PORT : 5000;
+const port = process.env.NODE_ENV === "production" ? process.env.PORT : 3000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+app.use(express.static("frontend/build"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -27,6 +25,6 @@ app.get("/test", (req, res) => {
 app.use("/api/auth", require("./routes/api/users"));
 app.use("/api/cred", require("./routes/api/credentials"));
 
-app.listen(3000, () => {
-  console.log(`Hello Jon, My server is running on Port ${3000}`);
+app.listen(port, () => {
+  console.log(`Server is running on Port ${port}`);
 });
